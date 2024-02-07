0c8e4b23f29f2251d8edd636528e091abdce2f23
Omar Shalaby <omarshalaby911@knight.ucf.edu>
2024-02-07 18:19:26 -0500

    retrieves and selects pokemon in tab

--- game-editor/frontend/src/App.tsx
+++ game-editor/frontend/src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
     }
     return (
         <div className="flex flex-col justify-evenly h-screen">
-            <h1 className="text-4xl">Welcome to The Pokemon Rom Hack Editor</h1>
+            <h1 className="text-4xl">Welcome to The Pokemon Game Engine</h1>
             <h2>Before continuing, Please set your settings folder to the base folder before continuing!</h2>
             <div className='flex w-screen justify-center'>
                 <button onClick={() => OptionsMenu()} className="file: bg-blueWhale rounded border-1 border-solid w-1/6 border-black">Select Folder</button>
--- game-editor/frontend/src/pokemon-editor/main.tsx
+++ game-editor/frontend/src/pokemon-editor/main.tsx
@@ -1,10 +1,111 @@
 import { useNavigate } from "react-router-dom";
+import { ParsePokemonData } from "../../wailsjs/go/main/App";
+import { useEffect, useState } from "react";
+import { Pokemon } from "./pokemon.model";
+
+// interface Pokemon {
+//     ID: string;
+//     Species: string;
+//     Types: string[];
+//     DexEntry: string;
+//     Abilities: {
+//         Name: string;
+//         isHidden: boolean;
+//     }[];
+//     Moves: {
+//         Name: string;
+//         Level: number;
+//         Method: string;
+//     }[];
+//     Evolutions: {
+//         Name: string;
+//         Methods: string[];
+//         ID: string;
+//     }[];
+//     Stats: {
+//         Hp: number;
+//         Attack: number;
+//         Defense: number;
+//         SpecialAttack: number;
+//         SpecialDefense: number;
+//         Speed: number;
+//     };
+//     AssetData: {
+//         Front: string;
+//         Back: string;
+//         ShinyFront: string;
+//         ShinyBack: string;
+//         Icon: string;
+//     };
+// }
+
+// interface Abilities {
+//     Name: string;
+//     isHidden: boolean;
+// }
+
+// interface Moves {
+//     Name: string;
+//     Level: number;
+//     Method: string;
+// }
+
+// interface Evolutions {
+//     Name: string;
+//     Methods: string[];
+//     ID: string;
+// }
+
+// interface Stats {
+//     Hp: number;
+//     Attack: number;
+//     Defense: number;
+//     SpecialAttack: number;
+//     SpecialDefense: number;
+//     Speed: number;
+// }
+
+// interface AssetData {
+//     Front: string;
+//     Back: string;
+//     ShinyFront: string;
+//     ShinyBack: string;
+//     Icon: string;
+// }
 
 export default function PokemonEditor():React.ReactElement {
     const navigate = useNavigate();
+    const [pokemonSpecies, setPokemonSpecies] = useState<Pokemon[]>([]);
+    const [selectedPokemon, setSelectedPokemon] = useState<Pokemon>();
+
+    const fetchPokemonSpecies = async() => {
+        let data = await ParsePokemonData();
+        setPokemonSpecies(data);
+    }
+
+    useEffect(() => {
+        fetchPokemonSpecies();
+        console.log("list of Pokemon: ", pokemonSpecies);
+    }, []);
+
     return (
-        <>
-            <button onClick={() => navigate(-1)}>Go back to main menu</button>
-        </>
-    );
+        <div>
+            <div>
+                <select
+                    onChange={(e) => {
+                        let selected = e.target.value;
+                        const selectedPokemonObj = pokemonSpecies.find(pokemon => pokemon.Name === selected);
+                        setSelectedPokemon(selectedPokemonObj);
+                        console.log(selectedPokemon);
+                    }}
+                >
+                    {pokemonSpecies.map((pokemon, index) => (
+                        <option key={index} value={pokemon.Name}>
+                            {pokemon.Name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+);
 }
