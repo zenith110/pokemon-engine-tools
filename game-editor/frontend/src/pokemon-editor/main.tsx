a4c75007db1a20bf02822c3e720c97dbda777c05
Fedbca123 <om091804@ucf.edu>
2024-02-14 15:38:12 -0500

    added Type pill and added margins

--- game-editor/frontend/src/pokemon-editor/main.tsx
+++ game-editor/frontend/src/pokemon-editor/main.tsx
@@ -20,19 +20,19 @@ export default function PokemonEditor():React.ReactElement {
     }, [selectedPokemon]);
 
     return (
-        <div className="flex flex-row h-dvh border-2 border-red-800">
+        <div className="flex flex-row h-[44rem] border-2 border-red-800 ml-2 mt-1">
 
-            <div className="h-screen flex flex-col border-4">
-                <div className="h-1/2  rounded-md grid grid-rows-2 grid-cols-2 items-stretch border-2 border-pink-400">
+            <div className="h-full flex flex-col border-4">
+                <div className="h-1/2  rounded-2xl grid grid-rows-2 grid-cols-2 items-stretch border-2 border-pink-400 bg-offWhite">
                     <img className="" src={selectedPokemon? `data:image/png;base64,${selectedPokemon?.FrontSprite}` : ''} alt="pokemon sprite" />
                     <img src={selectedPokemon? `data:image/png;base64,${selectedPokemon?.ShinyFront}` : ''} alt="pokemon sprite" />
                     <img src={selectedPokemon? `data:image/png;base64,${selectedPokemon?.BackSprite}` : ''} alt="pokemon sprite" />
-                    <img src={selectedPokemon? `data:image/png;base64,${selectedPokemon?.ShinyBack}` : ''} alt="pokemon sprite" />
+                    <img src={selectedPokemon ? `data:image/png;base64,${selectedPokemon?.ShinyBack}` : ''} alt="pokemon sprite" />
                 </div>
 
-                <div className="h-fit text-black border-2 flex flex-row justify-between pt-12">
+                <div className="h-fit text-black border-2 flex flex-row justify-around py-6">
                     <Select
-                        options={pokemonSpecies.map(pokemon => ({ value: pokemon.ID, label: pokemon.ID + ": " +pokemon.Name }))}
+                        options={pokemonSpecies.map(pokemon => ({ value: pokemon.ID, label: `${pokemon.ID}: ${pokemon.Name}`}))}
                         onChange={async (e) => {
                             const selected: Pokemon | undefined = pokemonSpecies.find(pokemon => pokemon.ID === (e?.value));
                             setSelectedPokemon(selected);
@@ -45,9 +45,19 @@ export default function PokemonEditor():React.ReactElement {
                         isRtl={false}
                         isSearchable={true}
                         isMulti={false}
-                        className="w-1/2"
+                        classNames={{
+                            control: () => "rounded-2xl"
+                        }}
                     />
-                    <button onClick={() => console.log("TO BE ADDED")} className="bg-blueWhale rounded hover:bg-wildBlueYonder w-5/12 text-white">New Pokemon</button>
+                    <button onClick={() => console.log("TO BE ADDED")} className="bg-blueWhale rounded-lg hover:bg-wildBlueYonder w-5/12 text-white">New Pokemon</button>
+                </div>
+
+                <div className="flex flex-row border-2 border-green-600 justify-center px-10">
+                    <div className="flex flex-row justify-between">
+                      <p className="bg-blueWhale px-6 py-3 rounded-l-2xl pl-10">Type:</p>
+                        <button onClick={() => {console.log("Add this later")}} className="bg-tealBlue px-6 py-3 rounded-r-2xl">{ selectedPokemon?.Types ? selectedPokemon.Types.join('/') : 'Dragon/Fighting' }</button>  
+                    </div>
+                    
                 </div>
 
                 <div className="h-3/12 pt-12 ">
