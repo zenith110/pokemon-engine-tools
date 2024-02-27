4afb73c0c832d1b544e1b6e347a1724650e0512c
zenith110 <thequake@live.com>
2024-02-26 21:30:03 -0500

    Added default values

--- game-editor/frontend/src/trainer-editor/main.jsx
+++ game-editor/frontend/src/trainer-editor/main.jsx
@@ -1,21 +1,26 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import React from "react";
-import { ParseTrainers } from "../../wailsjs/go/main/App";
+import { ParseTrainers, ParseTrainerClass } from "../../wailsjs/go/main/App";
 
 //\import { Trainer } from "./trainer.models";
 
 const TrainerEditor = () => {
     const [trainers, setTrainers] = useState([]);
     const [selectedTrainer, setSelectedTrainer] = useState();
+    const [classTypes, setClassTypes] = useState([])
     const navigate = useNavigate();
     useEffect(() => { 
         const fetchTrainers = async() => {
             let data = await ParseTrainers()
             setTrainers(data)
         }
+        const fetchClassTypes = async() => {
+            let data = await ParseTrainerClass()
+            setClassTypes(data.Data)
+        }
         fetchTrainers()
-        
+        fetchClassTypes()
     }, []) 
     return(
         <>
@@ -24,7 +29,7 @@ const TrainerEditor = () => {
                 const trainerData = trainers.find((trainer) => trainer.id === e.target.value);
                 setSelectedTrainer(trainerData);
            }} defaultValue={"placeholder"}>
-            <option value={"placeholder"} disabled>Select a trainer sprite</option>
+            <option value={"placeholder"} disabled>Select a trainer</option>
             {trainers?.map((trainer) =>
                 <option value={trainer.id} key={trainer.id}>{trainer.name}</option>
             )}
@@ -33,15 +38,31 @@ const TrainerEditor = () => {
             <div className="text-black flex items-center justify-center">
                 <img src={selectedTrainer? `data:image/png;base64,${selectedTrainer?.sprite}` : ''} alt="Trainer Sprite" />
             </div>
+            <br/>
             <div className="text-white flex items-center justify-center">
-                <label>Trainer name: {selectedTrainer?.name? selectedTrainer.name : ''}</label>
+                <label>Trainer name: </label>
             </div>
-            <label>Class type: {selectedTrainer?.classType? selectedTrainer.classType : ''}</label>
+            <div className="text-black flex items-center justify-center">
+                <input defaultValue={selectedTrainer?.name? selectedTrainer.name : ''}/>
+            </div>
+            <br/>
+            <label>Trainer class</label>
+            <div className="text-black flex items-center justify-center">
+                <select name="trainerClasses" defaultValue={selectedTrainer?.classType? selectedTrainer.classType : ''}>
+                <option value={"placeholder"} disabled>Select a trainer class</option>
+                {classTypes.map((trainerClass) =>
+                    <option value={trainerClass.Name} key={trainerClass.Name}>{trainerClass.Name}</option>
+                )}
+                </select>
+            </div>
             {selectedTrainer?.pokemons.map((pokemon) =>
                 <div className="text-black flex items-center justify-center">
                     <img src={pokemon? `data:image/png;base64,${pokemon?.front}` : ''} alt="Sprite" />
                 </div>
             )}
+           <br/>
+           <button className="file: bg-blueWhale rounded border-1 border-solid w-1/6 border-black">Save</button>
+           <br/>
            <br/>
            <button onClick={() => navigate("new-trainer")} className="file: bg-blueWhale rounded border-1 border-solid w-1/6 border-black">New trainer</button>
            <br/>
