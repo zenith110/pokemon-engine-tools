3cdbc8043305adaf7dc96b9396ae0509fd3e71a5
zenith110 <thequake@live.com>
2024-04-04 21:00:32 -0400

    Added code for move editor

--- game-editor/move-editor.go
+++ game-editor/move-editor.go
@@ -34,7 +34,7 @@ func (a *App) ParseMoves() Models.AllMoves {
 	return moves
 }
 
-func (a *App) UpdateMove(UpdatedMove) {
+func (a *App) UpdateMove(updatedMove UpdatedMove) {
 	file, err := os.Open(fmt.Sprintf("%s/data/toml/moves.toml", a.dataDirectory.DataDirectory))
 	if err != nil {
 		log.Fatalf("Error has occured while opening file to edit %v", err)
@@ -50,38 +50,13 @@ func (a *App) UpdateMove(UpdatedMove) {
 	if err != nil {
 		fmt.Printf("Error has occured reading unmarshling into struct %v", err)
 	}
-	for move := range moves.Move {
-		if moves.Move[move].Id == trainerJson.Id {
-			var pokemons []Models.Pokemons
-			for pokemonIndex := range trainerJson.Pokemons {
-				pokemon := Models.Pokemons{
-					ID:             trainerJson.Pokemons[pokemonIndex].ID,
-					HP:             trainerJson.Pokemons[pokemonIndex].HP,
-					Species:        trainerJson.Pokemons[pokemonIndex].Species,
-					Speed:          trainerJson.Pokemons[pokemonIndex].Speed,
-					Attack:         trainerJson.Pokemons[pokemonIndex].Attack,
-					Defense:        trainerJson.Pokemons[pokemonIndex].Defense,
-					SpecialAttack:  trainerJson.Pokemons[pokemonIndex].SpecialAttack,
-					SpecialDefense: trainerJson.Pokemons[pokemonIndex].SpecialDefense,
-					Moves:          trainerJson.Pokemons[pokemonIndex].Moves,
-					Level:          trainerJson.Pokemons[pokemonIndex].Level,
-					HeldItem:       trainerJson.Pokemons[pokemonIndex].HeldItem,
-				}
-				pokemons = append(pokemons, pokemon)
-			}
-			trainers.Trainers[trainer].Name = trainerJson.Name
-			trainers.Trainers[trainer].Pokemons = pokemons
-			trainers.Trainers[trainer].ClassType = trainerJson.ClassType
-			trainers.Trainers[trainer].Sprite = trainerJson.Sprite
-		}
-	}
 
-	data, err := toml.Marshal(trainers)
+	data, err := toml.Marshal(moves)
 	if err != nil {
-		panic(fmt.Errorf("error had occured while creating trainer data!\n%v", err))
+		panic(fmt.Errorf("error had occured while creating move data!\n%v", err))
 	}
-	os.Remove(fmt.Sprintf("%s/data/toml/trainers.toml", a.dataDirectory.DataDirectory))
-	f, err := os.OpenFile(fmt.Sprintf("%s/data/toml/trainers.toml", a.dataDirectory.DataDirectory), os.O_CREATE, 0644)
+	os.Remove(fmt.Sprintf("%s/data/toml/moves.toml", a.dataDirectory.DataDirectory))
+	f, err := os.OpenFile(fmt.Sprintf("%s/data/toml/moves.toml", a.dataDirectory.DataDirectory), os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalf("Error occured while opening file %v", err)
 	}
