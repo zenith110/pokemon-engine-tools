1383ac77b74866b3a61c31b541e62f8fb200a66b
zenith110 <thequake@live.com>
2024-04-28 20:26:06 -0400

    Added svgs to render

--- game-editor/frontend/src/overworld-editor/new-overworlds/NewOverworlds.jsx
+++ game-editor/frontend/src/overworld-editor/new-overworlds/NewOverworlds.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import DownArrow from "../images/reshot-icon-down-arrow-P6BUA8L4DS.svg";
+import UpArrow from "../images/reshot-icon-up-arrow-XMEL8JGW5T.svg";
+import LeftArrow from "../images/reshot-icon-left-arrow-2RFCAW584E.svg";
+import RightArrow from "../images/reshot-icon-right-arrow-5E3R279NU8.svg";
 const NewOverworlds = () => {
     const [name, setName] = useState("");
     const [swimmingFrames, setSwimmingFrames] = useState([])
@@ -7,43 +11,48 @@ const NewOverworlds = () => {
     const [runningFrames, setRunningFrames] = useState([])
     const [isPlayer, setIsPlayer] = useState(false);
     const [surfingFrames, setSurfingFrames] = useState([])
-    const [ID, setID] = useState("");    
+    const [ID, setID] = useState("");
+    const [typeOfFrame, setTypeOfFrame] = useState();
     return(
         <div>
             <div>
+                <label>Name of OW</label>
+                <br/>
+                <input onChange={(e) => setName(e.target.value)}></input>
+                <br/>
                 <label>Swimming frames</label>
                 <br/>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button><img src={UpArrow} width={32} height={32}/></button>
+                <button><img src={DownArrow} width={32} height={32}/></button>
+                <button><img src={LeftArrow} width={32} height={32}/></button>
+                <button><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/> 
             <div>
                 <label>Running frames</label>
                 <br/>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button><img src={UpArrow} width={32} height={32}/></button>
+                <button><img src={DownArrow} width={32} height={32}/></button>
+                <button><img src={LeftArrow} width={32} height={32}/></button>
+                <button><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/>
             <div>
                 <label>Walking frames</label>
                 <br/>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button><img src={UpArrow} width={32} height={32}/></button>
+                <button><img src={DownArrow} width={32} height={32}/></button>
+                <button><img src={LeftArrow} width={32} height={32}/></button>
+                <button><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/>
             <div>
                 <label>Surfing frames</label>
                 <br/>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <button><img src={UpArrow} width={32} height={32}/></button>
+                <button><img src={DownArrow} width={32} height={32}/></button>
+                <button><img src={LeftArrow} width={32} height={32}/></button>
+                <button><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/>
             <button onClick={() => {
