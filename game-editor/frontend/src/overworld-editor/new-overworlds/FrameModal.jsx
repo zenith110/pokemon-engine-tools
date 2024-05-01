fcb2f633c223ad54ab527ca2851b7d1dbf2c0406
zenith110 <thequake@live.com>
2024-04-30 20:00:16 -0400

    Got basic modal popping up for the ow editor

--- game-editor/frontend/src/overworld-editor/new-overworlds/FrameModal.jsx
+++ game-editor/frontend/src/overworld-editor/new-overworlds/FrameModal.jsx
@@ -1,4 +1,29 @@
-const FrameModal = ({ typeOfFrame, nameOfOW, setFrames, Direction}) => {
- 
+import Modal from 'react-modal';
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+const FrameModal = ({ typeOfFrame, nameOfOW, setFrames, direction, modalIsOpen, closeModal}) => {
+ return(
+    <div>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+        >
+        <div className="text-black">
+            <button onClick={() => closeModal()}>Close</button>
+        </div>
+        </Modal>
+    </div>
+ )
 } 
 export default FrameModal;
--- game-editor/frontend/src/overworld-editor/new-overworlds/NewOverworlds.jsx
+++ game-editor/frontend/src/overworld-editor/new-overworlds/NewOverworlds.jsx
@@ -14,6 +14,30 @@ const NewOverworlds = () => {
     const [runningFrames, setRunningFrames] = useState([]);
     const [isPlayer, setIsPlayer] = useState(false);
     const [surfingFrames, setSurfingFrames] = useState([]);
+    const [modalIsOpen, setIsOpen] = useState(false);
+    const [frameSet, setFrameSet] = useState();
+    const [currentDirection, setCurrentDirection] = useState()
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+    const renderFrameModal = (frameType) => {
+        switch(frameType) {
+            case 'swimming':
+                return <FrameModal typeOfFrame={"swimming"} nameOfOW={name} setFrames={setSwimmingFrames} direction={currentDirection} modalIsOpen={modalIsOpen} closeModal={closeModal}/>;
+            case 'running':
+                return <FrameModal typeOfFrame={"running"} nameOfOW={name} setFrames={setRunningFrames} direction={currentDirection} modalIsOpen={modalIsOpen} closeModal={closeModal}/>;
+            case 'walking':
+                return <FrameModal typeOfFrame={"walking"} nameOfOW={name} setFrames={setWalkingFrames} direction={currentDirection} modalIsOpen={modalIsOpen} closeModal={closeModal}/>;
+            case 'surfing':
+                return <FrameModal typeOfFrame={"surfing"} nameOfOW={name} setFrames={setSurfingFrames} direction={currentDirection} modalIsOpen={modalIsOpen} closeModal={closeModal}/>;  
+            default:
+                return '';
+        }
+    }
     return(
         <div>
             <div>
@@ -24,16 +48,24 @@ const NewOverworlds = () => {
                 <label>Swimming frames</label>
                 <br/>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Swimming"} nameOfOW={name} setFrames={setSwimmingFrames} Direction={"Up"}/>
+                    openModal();
+                    setCurrentDirection("up");
+                    setFrameSet("swimming");
                 }}><img src={UpArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Swimming"} nameOfOW={name} setFrames={setSwimmingFrames} Direction={"Down"}/>
+                    openModal();
+                    setCurrentDirection("down");
+                    setFrameSet("swimming");
                 }}><img src={DownArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Swimming"} nameOfOW={name} setFrames={setSwimmingFrames} Direction={"Left"}/>
+                    openModal();
+                    setCurrentDirection("left");
+                    setFrameSet("swimming");
                 }}><img src={LeftArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Swimming"} nameOfOW={name} setFrames={setSwimmingFrames} Direction={"Right"}/>
+                   openModal();
+                   setCurrentDirection("right");
+                   setFrameSet("swimming");
                 }}><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/> 
@@ -41,16 +73,24 @@ const NewOverworlds = () => {
                 <label>Running frames</label>
                 <br/>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Running"} nameOfOW={name} setFrames={setRunningFrames} Direction={"Up"}/>
+                   openModal();
+                   setCurrentDirection("up");
+                   setFrameSet("running");
                 }}><img src={UpArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Running"} nameOfOW={name} setFrames={setRunningFrames} Direction={"Down"}/>
+                    openModal();
+                    setCurrentDirection("down");
+                    setFrameSet("running");
                 }}><img src={DownArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Running"} nameOfOW={name} setFrames={setRunningFrames} Direction={"Left"}/>
+                    openModal();
+                    setCurrentDirection("left");
+                    setFrameSet("running");
                 }}><img src={LeftArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Running"} nameOfOW={name} setFrames={setRunningFrames} Direction={"Right"}/>
+                    openModal();
+                    setCurrentDirection("right");
+                    setFrameSet("running");
                 }}><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/>
@@ -58,16 +98,24 @@ const NewOverworlds = () => {
                 <label>Walking frames</label>
                 <br/>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Walking"} nameOfOW={name} setFrames={setWalkingFrames} Direction={"Up"}/>
+                    openModal();
+                    setCurrentDirection("up");
+                    setFrameSet("walking");
                 }}><img src={UpArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Walking"} nameOfOW={name} setFrames={setWalkingFrames} Direction={"Down"}/>
+                    openModal();
+                    setCurrentDirection("down");
+                    setFrameSet("walking");
                 }}><img src={DownArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Walking"} nameOfOW={name} setFrames={setWalkingFrames} Direction={"Left"}/>
+                    openModal();
+                    setCurrentDirection("left");
+                    setFrameSet("walking");
                 }}><img src={LeftArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Walking"} nameOfOW={name} setFrames={setWalkingFrames} Direction={"Right"}/>
+                    openModal();
+                    setCurrentDirection("right");
+                    setFrameSet("walking");
                 }}><img src={RightArrow} width={32} height={32}/></button>
             </div>
             <br/>
@@ -75,20 +123,29 @@ const NewOverworlds = () => {
                 <label>Surfing frames</label>
                 <br/>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Surfing"} nameOfOW={name} setFrames={setSurfingFrames} Direction={"Up"}/>
+                    openModal();
+                    setCurrentDirection("up");
+                    setFrameSet("surfing");
                 }}><img src={UpArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Surfing"} nameOfOW={name} setFrames={setSurfingFrames} Direction={"Down"}/>
+                    openModal();
+                    setCurrentDirection("down");
+                    setFrameSet("surfing");
                 }}><img src={DownArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Surfing"} nameOfOW={name} setFrames={setSurfingFrames} Direction={"Left"}/>
+                    openModal();
+                    setCurrentDirection("left");
+                    setFrameSet("surfing");
                 }}><img src={LeftArrow} width={32} height={32}/></button>
                 <button onClick={() => {
-                    <FrameModal typeOfFrame={"Surfing"} nameOfOW={name} setFrames={setSurfingFrames} Direction={"Right"}/>
+                    openModal();
+                    setCurrentDirection("right");
+                    setFrameSet("surfing");
                 }}><img src={RightArrow} width={32} height={32}/></button>
                 <br/>
                 <input type="checkbox" id="playerChoice" name="playerChoice" value="Player"/>
-                <label for="playerChoice">Is a playable character</label>
+                <label htmlFor="playerChoice">Is a playable character</label>
+            {renderFrameModal(frameSet)}
             </div>
             <br/>
             <button onClick={() => {
