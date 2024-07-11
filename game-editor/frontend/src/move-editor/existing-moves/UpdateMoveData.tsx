f595c3f049827d1738535733ead0eb9813fcb558
zenith110 <thequake@live.com>
2024-07-10 21:30:02 -0400

    Updated move editor to be tsx

---
+++ game-editor/frontend/src/move-editor/existing-moves/UpdateMoveData.tsx
@@ -0,0 +1,89 @@
+import { UpdateMove } from "../../../wailsjs/go/main/App";
+import { Dispatch, SetStateAction } from "react";
+import { Move } from "../move.model";
+
+const UpdateMoveData = ({
+  selectedMove,
+  power,
+  setPower,
+  pp,
+  setPP,
+  accuracy,
+  setAccuracy,
+  moveType,
+  setMoveType,
+  name,
+  setName,
+}: {
+  selectedMove: Move;
+  power: number | undefined;
+  setPower: Dispatch<SetStateAction<number | undefined>>;
+  pp: number | undefined;
+  setPP: Dispatch<SetStateAction<number | undefined>>;
+  accuracy: number | undefined;
+  setAccuracy: Dispatch<SetStateAction<number | undefined>>;
+  moveType: string | undefined;
+  setMoveType: Dispatch<SetStateAction<string | undefined>>;
+  name: string | undefined;
+  setName: Dispatch<SetStateAction<string | undefined>>;
+}) => {
+  return (
+    <div>
+      <br />
+      <br />
+      <label>Power: </label>
+      <input
+        type="number"
+        value={power}
+        onChange={(e) => setPower(parseInt(e.target.value))}
+      ></input>
+      <br />
+      <label>PP: </label>
+      <input
+        type="number"
+        value={pp ? pp : 0}
+        onChange={(e) => setPP(parseInt(e.target.value))}
+      ></input>
+      <br />
+      <label>Accuracy: </label>
+      <input
+        type="number"
+        value={accuracy ? accuracy : 0}
+        onChange={(e) => setAccuracy(parseInt(e.target.value))}
+      ></input>
+      <br />
+      <label>Type: </label>
+      <input
+        value={moveType ? moveType : ""}
+        onChange={(e) => setMoveType(e.target.value)}
+      ></input>
+      <br />
+      <label>Name: </label>
+      <input
+        value={name ? name : ""}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <br />
+      <br />
+      <button
+        className="file: bg-blueWhale rounded border-1 border-solid w-1/6 border-black text-white"
+        onClick={async () => {
+          // Weird cast, need to revisit better way of doing this.
+          let data = {
+            power: Number(power),
+            pp: Number(pp),
+            accuracy: Number(accuracy),
+            type: String(moveType),
+            name: String(name),
+            id: selectedMove?.ID.toString(),
+            description: "test",
+          };
+          await UpdateMove(data);
+        }}
+      >
+        Save
+      </button>
+    </div>
+  );
+};
+export default UpdateMoveData;
---
+++ game-editor/frontend/src/move-editor/main.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { ParseMoves } from "../../wailsjs/go/main/App";
+import Select from "react-select";
+import UpdateMoveData from "./existing-moves/UpdateMoveData";
+import { Move } from "./move.model";
+
+const MoveEditor = () => {
+  const [moves, setMoves] = useState<Move[]>([]);
+  const [selectedMove, setSelectedMove] = useState<Move>();
+  const [power, setPower] = useState<number | undefined>(0);
+  const [pp, setPP] = useState<number | undefined>(0);
+  const [accuracy, setAccuracy] = useState<number | undefined>(0);
+  const [moveType, setMoveType] = useState<string | undefined>("");
+  const [name, setName] = useState<string | undefined>("");
+
+  useEffect(() => {
+    const fetchMoves = async () => {
+      let data = await ParseMoves();
+      setMoves(data.Move);
+    };
+    fetchMoves();
+  }, []);
+  return (
+    <div className="text-black">
+      <Select
+        name="moves"
+        onChange={(e) => {
+          const move: Move | undefined = moves.find(
+            (moveData) => moveData.ID === e?.value
+          );
+          setSelectedMove(move);
+          setPower(move?.Power);
+          setPP(move?.Pp);
+          setAccuracy(move?.Accuracy);
+          setMoveType(move?.Type);
+          setName(move?.Name);
+        }}
+        isClearable={false}
+        isDisabled={false}
+        isLoading={false}
+        isRtl={false}
+        isSearchable={true}
+        isMulti={false}
+        classNames={{
+          control: () => "rounded-2xl",
+        }}
+        options={moves?.map((move) => ({
+          value: move.ID,
+          label: `${move.Name}`,
+        }))}
+      />
+      {selectedMove ? (
+        <UpdateMoveData
+          selectedMove={selectedMove}
+          power={power}
+          setPower={setPower}
+          pp={pp}
+          setPP={setPP}
+          accuracy={accuracy}
+          setAccuracy={setAccuracy}
+          moveType={moveType}
+          setMoveType={setMoveType}
+          name={name}
+          setName={setName}
+        />
+      ) : (
+        <div />
+      )}
+      <br />
+      <button className="file: bg-blueWhale rounded border-1 border-solid w-1/6 border-black text-white">
+        New Move
+      </button>
+    </div>
+  );
+};
+export default MoveEditor;
